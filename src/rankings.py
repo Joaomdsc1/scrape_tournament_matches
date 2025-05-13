50f5433c22a5f20a51d3939491461bba2fd37ea0
Joao Guilherme Marcondes de Souza Costa <jgmarcondes34@gmail.com>
2025-05-13 18:48:17 -0300

    extraindo o html das tabelas

--- src/rankings.py
+++ src/rankings.py
@@ -2,25 +2,36 @@ import os
 from playwright.sync_api import sync_playwright
 from bs4 import BeautifulSoup
 
+paths = [
+        "/football/brazil/serie-a-betano/",
+        "/football/germany/bundesliga/",
+        "/football/brazil/serie-b-superbet/",
+        "/football/england/premier-league/",
+        "/football/italy/serie-a/",
+        "/basketball/usa/nba/",
+        "/basketball/brazil/nbb/"
+    ]
+
 # Garante que o diretório exista
 output_dir = 'data/standings/hmtl'
 os.makedirs(output_dir, exist_ok=True)
-output_path = os.path.join(output_dir, 'serie_a_table.html')
+for path in paths:
+    output_path = os.path.join(output_dir, "/".join(path.split("/")[3:4])+'.html')
 
-with sync_playwright() as p:
-    browser = p.chromium.launch(headless=True)
-    page = browser.new_page()
-    page.goto("https://www.betexplorer.com/football/brazil/serie-a-betano/")
-    page.wait_for_timeout(5000)
-    html = page.content()
-    browser.close()
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)
+        page = browser.new_page()
+        page.goto("https://www.betexplorer.com" + path)
+        page.wait_for_timeout(5000)
+        html = page.content()
+        browser.close()
 
-soup = BeautifulSoup(html, 'html.parser')
-table = soup.find('table', id='table-type-1')
+    soup = BeautifulSoup(html, 'html.parser')
+    table = soup.find('table', id='table-type-1')
 
-if table:
-    with open(output_path, 'w', encoding='utf-8') as f:
-        f.write(table.prettify())
-    print(f"Tabela salva em '{output_path}'")
-else:
-    print("Tabela não encontrada.")
+    if table:
+        with open(output_path, 'w', encoding='utf-8') as f:
+            f.write(table.prettify())
+        print(f"Tabela salva em '{output_path}'")
+    else:
+        print("Tabela não encontrada.")
